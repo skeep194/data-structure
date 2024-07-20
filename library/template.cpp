d3bb5d2b470b3efd769356621449ef17642a9d2e
Jongun Jeong <skeep194@knu.ac.kr>
2024-07-21 00:04:57 +0900

    new templete

--- library/template.cpp
+++ library/template.cpp
@@ -2,32 +2,12 @@
 using namespace std;
 #define fastio cin.tie(0)->sync_with_stdio(0);
 // typedef __int128_t i128;
-typedef int64_t i64;
-typedef int32_t i32;
-const int INTINF = numeric_limits<i32>::max();
-const long long LONGINF = numeric_limits<i64>::max();
+typedef long long lint;
+typedef long double ld;
 template <typename T>
 inline T midpoint(const T &a, const T &b) { return a + (b - a) / 2; }
-// #define MULTITC
 #define endl '\n'
 // #define int long long
-void solve(int tc);
-void input();
-int32_t main()
-{
-    int tc = 1;
-#ifndef LOCAL
-    fastio;
-#endif
-#ifdef MULTITC
-    cin >> tc;
-#endif
-    for (int i = 1; i <= tc; ++i)
-    {
-        input();
-        solve(i);
-    }
-}
 template <typename T>
 istream &operator>>(istream &is, vector<T> &arr)
 {
@@ -42,12 +22,8 @@ ostream &operator<<(ostream &os, vector<T> &arr)
         os << i << ' ';
     return os;
 }
-// saigo nankade sonna yume watashi tsuburarinn..
+// plz accept my solution
 
-void input()
-{
-}
-
-void solve(int tc)
+int32_t main()
 {
 }
